50643f963c7ae461f0b0ed2dde4140330b1e571b
ulisescastell <ulises.castell.7e7@itb.cat>
2024-10-19 16:45:39 +0200

    first commit

---
+++ src/components/ComputedComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import {ref, computed} from 'vue'
+
+    const name = ref('ulises')
+    const upperName = computed(() => name.value.toUpperCase())
+</script>
+
+<template>
+    <section>
+        <h2>Computed example</h2>
+        <small> Computed makes dynamics variables. </small>
+        <div>
+            <p>The value is <span>{{ name }}</span> and the computed value is <span>{{ upperName }}</span></p>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    span {
+        font-weight: bold;
+    }
+</style>
